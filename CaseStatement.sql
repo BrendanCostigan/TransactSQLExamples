d8ee570ef12e30a49d6c7285645f6d525653dc34
Brendan Costigan <Brendan Costigan>
2016-10-03 15:04:46 +0100

    Inital commit

---
+++ CaseStatement.sql
@@ -0,0 +1,18 @@
+
+-- Simple Case
+
+SELECT CASE 2
+	WHEN 1 THEN 'One'
+	WHEN 2 THEN 'Two'
+	ELSE 'Else option'
+END
+
+
+-- Search Case
+
+SELECT CASE	
+		WHEN 2 = 1 THEN 'One'
+		WHEN 2 = 2 THEN 'Two'
+	ELSE 'Else option'
+END
+		 
