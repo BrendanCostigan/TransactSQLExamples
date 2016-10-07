6d99180aeabe68645b364042e3e07f195fc52a90
Brendan <costiganb@gmail.com>
2016-10-07 06:32:16 +0100

    Initial version

---
+++ ColumnstoreIndexExample.sql
@@ -0,0 +1,46 @@
+
+
+
+USE tempdb;
+GO
+
+IF OBJECT_ID('SimpleTable', 'U') IS NOT NULL
+	DROP TABLE SimpleTable;
+
+CREATE TABLE SimpleTable
+(
+	ProductKey		int NOT NULL,
+	OrderDateKey	int NOT NULL,
+	DueDateKey		int NOT NULL,
+	ShipDateKey		int NOT NULL
+);
+GO
+
+-- This example was originally written for SQL 2012. I recall seeing that significant changes were made in SQL 2104 which may make the following comments invalid in versions post 2012.
+
+-- Columnstore index cannot be clustered
+-- Columnstore index cannot be a unique index
+-- Columnstore index cannot act as a primary key or a foreign key
+CREATE CLUSTERED INDEX cl_simple ON SimpleTable (ProductKey);
+GO
+
+CREATE NONCLUSTERED COLUMNSTORE INDEX csindx_simple
+ON SimpleTable
+(
+	OrderDateKey,
+	DueDateKey,
+	ShipDateKey
+);
+GO
+
+
+--!! SQL 2012 ERROR - INSERT statement failed because data cannot be updated in a table with a columnstore index. Consider disabling the columnstore index before issuing the INSERT statement, then rebuilding the columnstore index after INSERT is complete.
+--!! SQL 2016 - This statement now works.
+--INSERT SimpleTable
+--VALUES (1, 2, 3, 4);
+
+
+-- Tidy up
+IF OBJECT_ID('SimpleTable', 'U') IS NOT NULL
+	DROP TABLE SimpleTable;
+
