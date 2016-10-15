327088354da1bdb488b5d5ce88476f6d39138cb7
Brendan <costiganb@gmail.com>
2016-10-15 08:16:28 +0100

    Added comment on similarities with PATINDEX

--- CharindexExample.sql
+++ CharindexExample.sql
@@ -1,6 +1,8 @@
 
 -- CHARINDEX: Searches an expression for another expression and returns its starting position if found.
 
+-- Similar to PATINDEX but CHARINDEX cannot be used with text, ntext, and image data types. PATINDEX uses wildcards and therefore can search for more complex search patterns.
+
 -- If the string is found then returns starting location of string searched for.
 SELECT CHARINDEX('quick', 'The quick brown fox jumps over a lazy dog.');
 
