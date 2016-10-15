e0a4b7dca39ab0cc0a6a5c1f5b7e5b619c27f02f
Brendan <costiganb@gmail.com>
2016-10-15 08:54:38 +0100

    Initial version

--- Coalesce.sql
+++ Coalesce.sql
@@ -1,6 +1,12 @@
 
 -- Evaluates the arguments in order and returns the current value of the first expression that initially does not evaluate to NULL.
 
-SELECT COALESCE(NULL, 'Fred');
+SELECT COALESCE(NULL, 'Fred', 'Joe');  --< Returns Fred
 
-SELECT COALESCE('Joe', 'Fred');
+SELECT COALESCE('Joe', 'Fred', 'Mary'); --< Returns Joe
+
+
+-- ISNULL Non-standard t-SQL alterative which is limited to two parameters. Do not use.
+
+SELECT ISNULL(NULL, 'Fred');  --< Returns Fred
+
