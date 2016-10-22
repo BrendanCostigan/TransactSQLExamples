6a35c753a53fbeccfd6ceacb3f0b1f00f9b024f2
Brendan Costigan <costiganb@gmail.com>
2016-10-22 08:04:45 +0100

    Clarify column headings

--- PartitionExample.sql
+++ PartitionExample.sql
@@ -6,9 +6,9 @@ SET NOCOUNT ON;				--< The WITH clause would with fail without a semicolon
 WITH CTE AS
 (
 	SELECT
-	ROW_NUMBER() OVER(PARTITION BY ProductLine ORDER BY ListPrice, productid) AS rownum, ProductLine, productid, Name, ListPrice
+	ROW_NUMBER() OVER(PARTITION BY ProductLine ORDER BY ListPrice, productid) AS RowNumber, ProductLine, productid, Name, ListPrice
 	FROM Production.Product
 )
-SELECT rownum, ProductLine, productid, Name, ListPrice
+SELECT RowNumber, ProductLine, productid, Name, ListPrice
 FROM CTE
-WHERE rownum <= 2;
+WHERE RowNumber <= 2;
