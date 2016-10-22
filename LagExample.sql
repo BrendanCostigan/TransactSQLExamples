a2d1b04377d35fb5a06768f988eefb456e4cb735
Brendan Costigan <costiganb@gmail.com>
2016-10-22 08:56:23 +0100

    Added FIRST_VALUE and LAST_VALUE examples

--- LagExample.sql
+++ LagExample.sql
@@ -7,7 +7,12 @@ SELECT	TOP 10
 		[SalesOrderID], 
 		[OrderDate], 
 		[TotalDue],
-		LAG([TotalDue], 1, 0) OVER(PARTITION BY [CustomerID] ORDER BY [OrderDate], [SalesOrderID]) AS PreviousValue,
-		LEAD([TotalDue], 1, 0) OVER(PARTITION BY [CustomerID] ORDER BY [OrderDate], [SalesOrderID]) AS NextValue
+		LAG([TotalDue]) OVER(PARTITION BY [CustomerID] ORDER BY [OrderDate], [SalesOrderID]) AS PreviousValue, 
+		LEAD([TotalDue], 1, 0) OVER(PARTITION BY [CustomerID] ORDER BY [OrderDate], [SalesOrderID]) AS NextValue,	--< Note default value of 0 rather than NULL, which is the default
+		FIRST_VALUE([TotalDue]) OVER(PARTITION BY [CustomerID] ORDER BY [OrderDate], [SalesOrderID]) AS FirstValue,
+		LAST_VALUE([TotalDue]) OVER(PARTITION BY [CustomerID] ORDER BY [OrderDate], [SalesOrderID]) AS QuestionableLastValue, --< Defaults to RANGE BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW which is probably not what you want
+		LAST_VALUE([TotalDue]) OVER(PARTITION BY [CustomerID] ORDER BY [OrderDate], [SalesOrderID] ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING) AS LastValue 
 FROM [Sales].[SalesOrderHeader]
-WHERE CustomerID = 11000;
+WHERE [CustomerID] = 11131
+ORDER BY CustomerID, [OrderDate], [SalesOrderID];
+
