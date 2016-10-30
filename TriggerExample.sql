680012e045ba122317e00be1c8ec9d6849ca2d3e
Brendan Costigan <costiganb@gmail.com>
2016-10-30 17:57:12 +0000

    Initial version

---
+++ TriggerExample.sql
@@ -0,0 +1,63 @@
+USE tempdb;
+GO
+
+IF OBJECT_ID('dbo.Products', 'U') IS NOT NULL
+	DROP TABLE dbo.Products;
+GO
+
+CREATE TABLE dbo.Products
+(
+	Id			INT,
+	Item		NVARCHAR(255)
+)
+GO
+
+-- AFTER trigger
+CREATE TRIGGER trUpdateAfterProduct
+ON dbo.Products
+AFTER INSERT, UPDATE, DELETE
+AS
+BEGIN
+	IF @@ROWCOUNT = 0 RETURN; 
+	
+	SELECT item FROM inserted;
+	
+	IF UPDATE(Item)
+		SELECT 'Column Item affected' AS Message;
+END
+GO	
+
+INSERT dbo.Products
+VALUES (1, 'Apple')		--< Above message produced for this INSERT
+
+UPDATE dbo.Products
+SET Item = 'Orange';	--< Above message produced for this UPDATE
+GO
+
+-- INSTEAD OF trigger
+CREATE TRIGGER trUpdateInsteadOfProduct
+ON dbo.Products
+INSTEAD OF INSERT, UPDATE, DELETE
+AS
+BEGIN
+	IF @@ROWCOUNT = 0 RETURN; 
+	
+	SELECT item FROM inserted;
+END
+GO	
+
+-- This insert will not work because the above INSTEAD OF trigger will fire
+INSERT dbo.Products
+VALUES (1, 'Pear')
+
+-- Note that Pear is not INSERTED
+SELECT * FROM dbo.Products
+GO
+
+-- Tidy up
+IF OBJECT_ID('dbo.Products', 'U') IS NOT NULL
+	DROP TABLE dbo.Products;
+GO
+
+
+
