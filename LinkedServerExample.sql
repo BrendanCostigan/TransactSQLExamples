96634ad74e22c48092f226fdfe63d6aba58d0200
Brendan Costigan <costiganb@gmail.com>
2016-11-02 17:53:55 +0000

    Initial version

---
+++ LinkedServerExample.sql
@@ -0,0 +1,28 @@
+-- On the instance with the resource on, in this case [HV-DEV\InstanceA]
+
+USE tempdb;
+GO
+
+IF OBJECT_ID(N'dbo.Cities', N'U') IS NOT NULL
+	DROP TABLE dbo.Cities;
+
+CREATE TABLE dbo.Cities
+(
+	Id			INT IDENTITY(1, 1) PRIMARY KEY,
+	CityName	NVARCHAR(255)
+)
+
+
+INSERT dbo.Cities (CityName)
+VALUES	('London'),
+		('Cardiff'),
+		('Belfast'),
+		('Edinburgh');
+
+
+-- On the server doing the calling
+
+
+sp_addlinkedserver [HV-DEV\InstanceA];
+
+SELECT * FROM [HV-DEV\InstanceA].tempdb.dbo.Cities
