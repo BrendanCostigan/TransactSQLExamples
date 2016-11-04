435fd6fca0f08d9ef141350efe9ce5dece3c6ca2
Brendan Costigan <costiganb@gmail.com>
2016-11-04 07:05:19 +0000

    Minor tidy up

--- ForeignKeyExample.sql
+++ ForeignKeyExample.sql
@@ -6,7 +6,7 @@ IF EXISTS (SELECT * FROM sys.objects WHERE name = 'foreign_key_example_sequence'
 GO	
 
 CREATE SEQUENCE [foreign_key_example_sequence]
-    AS int
+    AS INT
     START WITH 1
     INCREMENT BY 1
     MINVALUE 0
@@ -55,3 +55,14 @@ INSERT dbo.OrderDetail(OrderDate, ProductId, Quantity)
 VALUES (DEFAULT, 1, -1)				-- Default date, ProductId,	Quantity
 GO
 
+IF EXISTS (SELECT * FROM sys.objects WHERE name = 'OrderDetail' AND type = 'U')
+	DROP TABLE [dbo].[OrderDetail];
+GO	
+
+IF EXISTS (SELECT * FROM sys.objects WHERE name = 'Product' AND type = 'U')
+	DROP TABLE [dbo].[Product];
+GO	
+
+IF EXISTS (SELECT * FROM sys.objects WHERE name = 'foreign_key_example_sequence' AND type = 'SO')
+	DROP SEQUENCE [foreign_key_example_sequence];
+GO	
